cd215c97d12249f3152c51a20bc9a0b748bc85cc
WangWNico <nicowang0404@gmail.com>
2024-11-16 13:18:27 -0500

    Playable attack component

--- wolfjam2024/scenes/components/gravity_component.gd
+++ wolfjam2024/scenes/components/gravity_component.gd
@@ -7,4 +7,4 @@ func _physics_process(_delta: float) -> void:
 
 func handle_gravity(delta) -> void:
   if not target.is_on_floor():
-    target.velocity.y += ProjectSettings.get_setting("physics/2d/default_gravity") * delta
+	target.velocity.y += ProjectSettings.get_setting("physics/2d/default_gravity") * delta
--- wolfjam2024/scenes/components/health_component.gd
+++ wolfjam2024/scenes/components/health_component.gd
@@ -2,4 +2,3 @@ extends Node2D
 
 @export var target: Node2D
 @export var health := 100.0
-
