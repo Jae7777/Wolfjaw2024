1635490dddd0231f3c3d7e0692b1ddc1195a789d
WangWNico <nicowang0404@gmail.com>
2024-11-16 16:45:24 -0500

    Healthbar

--- wolfjam2024/scenes/components/health_component.gd
+++ wolfjam2024/scenes/components/health_component.gd
@@ -2,9 +2,16 @@ extends Node2D
 
 @export var target: Node2D
 @export var health := 3
-@export var hurtbox : CollisionShape2D
+@export var healthbar: ProgressBar
 
+func _ready() -> void:
+  set_health_bar()
+  
 func take_damage(damage: int) -> void:
   health -= damage
   if health <= 0:
     target.queue_free()
+  set_health_bar()
+    
+func set_health_bar() -> void:
+  healthbar.value = health
