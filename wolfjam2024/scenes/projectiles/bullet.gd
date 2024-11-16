4fffdae6b3cde065c5b16d6e94d401caff4d36f2
Justin Chen <jchen4086@gmail.com>
2024-11-16 15:44:23 -0500

    YESSSS

--- wolfjam2024/scenes/projectiles/bullet.gd
+++ wolfjam2024/scenes/projectiles/bullet.gd
@@ -6,6 +6,11 @@ var direction: Vector2
 var damage: int
 var speed: int
 
+func _ready() -> void:
+    position = spawn_position
+    # Optionally rotate the bullet sprite to face the direction of movement
+    rotation = direction.angle()
+
 func with_data(spawn_position_: Vector2, direction_: Vector2, damage_: int, speed_: int) -> Area2D:
     spawn_position = spawn_position_
     direction = direction_
@@ -13,17 +18,9 @@ func with_data(spawn_position_: Vector2, direction_: Vector2, damage_: int, spee
     speed = speed_
     return self
 
-func _ready() -> void:
-    position = spawn_position
-    # Optionally rotate the bullet sprite to face the direction of movement
-    rotation = direction.angle()
-
 func _physics_process(delta: float) -> void:
     # Move in the direction at constant speed
     position += direction * speed * delta
-    
-    # Optional: Delete bullet when it goes too far off screen
-    var viewport_rect = get_viewport_rect()
-    if !viewport_rect.has_point(position):
-        
-      
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+    queue_free()
