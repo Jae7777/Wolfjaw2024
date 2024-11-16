b5ead7a118addd9d864cb0a335fde8441555a8c2
WangWNico <nicowang0404@gmail.com>
2024-11-16 16:13:14 -0500

    Gun Effects Work

--- wolfjam2024/scenes/weapons/rifle.gd
+++ wolfjam2024/scenes/weapons/rifle.gd
@@ -1,8 +1,17 @@
 extends Node2D
 
+
+@onready var GunParticle = $Sprite2D/ProjectileSpawnPoint/GunParticle
+
+func _ready():
+  GunParticle.emitting = false
+  
 func _physics_process(_delta: float) -> void:
    pass
 
 func attack() -> void:
   print("KID ATTACK")
+  GunParticle.emitting = true
   $SpawnProjectileComponent.spawn_projectile()
+  await get_tree().create_timer(0.2).timeout  # Stop particles after 0.1 seconds
+  GunParticle.emitting = false
