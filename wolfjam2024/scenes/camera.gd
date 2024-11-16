b85271fbe21a83518e36c294db63e8392846d930
Christopher Poon <christopherpoonc@gmail.com>
2024-11-16 17:14:28 -0500

    animations

--- wolfjam2024/scenes/camera.gd
+++ wolfjam2024/scenes/camera.gd
@@ -4,15 +4,15 @@ extends Camera2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+    pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if target:
-		position += (target.global_position - position) * 0.1
+    if target:
+        position += (target.global_position - position) * 0.1
 
 func follow(new_target: CharacterBody2D) -> void:
-	target = new_target
-	var newPosition = target.global_position
-	position = lerp(position, newPosition, 0.1)
+    target = new_target
+    var newPosition = target.global_position
+    position = lerp(position, newPosition, 0.1)
--- wolfjam2024/scenes/components/playable_move_component.gd
+++ wolfjam2024/scenes/components/playable_move_component.gd
@@ -6,30 +6,37 @@ extends Node
 @export var base_speed := 350
 @export var run_speed := 10.0 * 60
 @export var defend_speed := 2.0 * 60
+@export var animated_sprite: AnimatedSprite2D
 
 var face_direction: int = 1
 
+
 func _physics_process(_delta: float) -> void:
-	target.move_and_slide()
+    target.move_and_slide()
 
 func jump() -> void:
-	target.velocity.y = -600
+    target.velocity.y = -600
 
 func move_left() -> void:
-	if dodge_timer.time_left == 0:
-		target.velocity.x = -base_speed
-		face_direction = -1
+    if dodge_timer.time_left == 0:
+        animated_sprite.play("walk")
+        target.velocity.x = -base_speed
+        face_direction = -1
+        animated_sprite.flip_h = true
 
 func move_right() -> void:
-	if dodge_timer.time_left == 0:
-		target.velocity.x = base_speed
-		face_direction = 1
+    if dodge_timer.time_left == 0:
+        animated_sprite.play("walk")
+        target.velocity.x = base_speed
+        face_direction = 1
+        animated_sprite.flip_h = false
 
 func reset_velocity() -> void:
-	if dodge_timer.time_left == 0:
-		target.velocity.x = 0
+    if dodge_timer.time_left == 0:
+        target.velocity.x = 0
+        animated_sprite.play("idle")
 
 func dodge() -> void:
-	if dodge_timer.time_left == 0:
-		target.velocity.x = base_speed * 2 * face_direction
-		dodge_timer.start()
+    if dodge_timer.time_left == 0:
+        target.velocity.x = base_speed * 2 * face_direction
+        dodge_timer.start()
