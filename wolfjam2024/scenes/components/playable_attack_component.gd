b90b8ece966ff0a6928f8d893b7a28f04414760f
Christopher Poon <christopherpoonc@gmail.com>
2024-11-16 20:03:18 -0500

    melee animation

--- wolfjam2024/scenes/components/playable_attack_component.gd
+++ wolfjam2024/scenes/components/playable_attack_component.gd
@@ -2,6 +2,9 @@ extends Node
 
 @export var target: CharacterBody2D
 @export var weapon: Node2D
+@export var animated_sprite: AnimatedSprite2D
 
 func attack() -> void:
+  animated_sprite.stop()
+  animated_sprite.play("melee")
   weapon.attack()
